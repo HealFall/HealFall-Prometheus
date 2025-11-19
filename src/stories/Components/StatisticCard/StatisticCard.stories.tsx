924f05ad5c10b835b2d0ecf6247142c93825d865
wangshoukang <wangshoukang@megaview.com>
2025-11-19 14:46:18 +0800

    feat: 数据统计卡片调整

--- src/stories/Components/StatisticCard/StatisticCard.stories.tsx
+++ src/stories/Components/StatisticCard/StatisticCard.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta } from "@storybook/react-vite";
 import StatisticCard from "./StatisticCard";
+import customIcon from "@/assets/StatictisCard/坐标.png";
+import { CloudDownloadOutlined } from "@ant-design/icons";
 
 const meta = {
   title: "组件/数据统计卡片",
@@ -14,5 +16,36 @@ export default meta;
 
 export const Default = {
   name: "StatisticCard",
-  render: () => <StatisticCard />,
+  args: {
+    title: "总数",
+    value: 20000,
+    prefix: "¥",
+    suffix: "元",
+    color: "black",
+    backgroundColor: "#f8f9ff",
+    imageSrc: customIcon,
+  },
+};
+
+export const Icon = {
+  name: "前后缀自定义图标",
+  args: {
+    title: "下载量",
+    value: "20,000",
+    prefix: <CloudDownloadOutlined />,
+    color: "black",
+    backgroundColor: "#f8f9ff",
+    imageSrc: customIcon,
+  },
+};
+
+export const Loading = {
+  name: "加载中",
+  args: {
+    title: "总数",
+    value: 200,
+    loading: true,
+    backgroundColor: "linear-gradient(135deg, #3b82f6, #8b5cf6)",
+    imageSrc: customIcon,
+  },
 };
--- src/stories/Components/StatisticCard/StatisticCard.tsx
+++ src/stories/Components/StatisticCard/StatisticCard.tsx
@@ -1,29 +1,106 @@
 import { Flex, Spin } from "antd";
 import styles from "./StatisticCard.module.less";
-import bgIllustration from "@/assets/StatictisCard/阿里巴巴矢量图标模块.png";
+import type { JSX } from "react";
 
 export interface StatisticCardProps {
+  /** 卡片标题 */
+  title: string;
+  /** 卡片数据 */
+  value: number | string;
+  /** 数据前缀 */
+  prefix?: string | JSX.Element;
+  /** 数据后缀 */
+  suffix?: string | JSX.Element;
   /** 加载中状态 */
   loading?: boolean;
+  /** 字体颜色 */
+  color?: string;
+  /** 背景颜色 */
+  backgroundColor?: string;
+  /** 外部传入的右侧图片 src（静态导入或链接） */
+  imageSrc?: string;
 }
 
-const StatisticCard = ({ loading = false }: StatisticCardProps) => {
+const StatisticCard = ({
+  title,
+  value,
+  prefix,
+  suffix,
+  loading = false,
+  color = "#000000",
+  backgroundColor = "#f8f9ff",
+  imageSrc,
+}: StatisticCardProps) => {
   return (
     <Spin spinning={loading}>
-      <Flex>
-        <div className={styles["card-container"]}>
-          <div className={styles["content-container"]}>
-            <div className={styles["statistic-value"]}>¥ 10000</div>
-            <div className={styles["statistic-title"]}>本年度培训学时总数</div>
-          </div>
-          <div className={styles["illustration-container"]}>
+      <Flex
+        align="center"
+        justify="center"
+        gap={20}
+        className={styles["card-container"]}
+        style={{ background: backgroundColor }}
+      >
+        {/* 左侧数据 */}
+        <Flex
+          vertical
+          align="flex-start"
+          justify="center"
+          flex={2}
+          className={styles["content-container"]}
+        >
+          {/* 上方数据 */}
+          <Flex
+            align="center"
+            justify="flex-start"
+            flex={2}
+            className={styles["statistic-value"]}
+            style={{ color: color }}
+          >
+            {/* 前缀及间距 */}
+            {prefix && (
+              <>
+                <span>{prefix}</span>
+                <span style={{ width: "10px" }}></span>
+              </>
+            )}
+            {/* 数据 */}
+            <span>{value}</span>
+            {/* 后缀及间距 */}
+            {suffix && (
+              <>
+                <span style={{ width: "10px" }}></span>
+                <span>{suffix}</span>
+              </>
+            )}
+          </Flex>
+          {/* 下方标题 */}
+          <Flex
+            align="center"
+            justify="flex-start"
+            flex={1}
+            className={styles["statistic-title"]}
+            style={{ color: color }}
+          >
+            {title}
+          </Flex>
+        </Flex>
+
+        {/* 右侧图标 */}
+        {imageSrc && (
+          <Flex
+            vertical
+            align="flex-start"
+            justify="center"
+            flex={1}
+            className={styles["illustration-container"]}
+          >
             <img
-              src={bgIllustration}
+              src={imageSrc}
               alt="illustration"
               className={styles["illustration-image"]}
             />
-          </div>
-        </div>
+          </Flex>
+        )}
       </Flex>
     </Spin>
   );
